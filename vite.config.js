1647484e7f78e886a5a4b1e1563f94179be62fef
medicinajvs <medicinajvs@gmail.com>
2026-01-30 11:31:18 -0300

    Alterando o caminho home do vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // SUBSTITUA 'nome-do-seu-repositorio' PELO NOME QUE ESTÁ NO GITHUB
 export default defineConfig({
   plugins: [react()],
-  base: '/Estrat-gia-MED/', 
+  base: '/medcofusmle/', 
 })
